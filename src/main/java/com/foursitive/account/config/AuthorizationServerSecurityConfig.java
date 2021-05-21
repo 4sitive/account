dfd1591249b472a7e23d87497fb9a2d2dc3f578d
이경욱 <kyungwook.yi@gmail.com>
2021-05-21 11:27:36 +0900

    인증 토큰 암호화 키 고정

--- src/main/java/com/foursitive/account/config/AuthorizationServerSecurityConfig.java
+++ src/main/java/com/foursitive/account/config/AuthorizationServerSecurityConfig.java
@@ -2,10 +2,12 @@ package com.foursitive.account.config;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
-import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.SneakyThrows;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -19,41 +21,35 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenCustomizer;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.web.OAuth2AuthorizationEndpointFilter;
 import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.util.ReflectionUtils;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.math.BigInteger;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.ECKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.ECFieldFp;
-import java.security.spec.ECParameterSpec;
-import java.security.spec.ECPoint;
-import java.security.spec.EllipticCurve;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Duration;
 import java.util.Optional;
 import java.util.UUID;
 
 @Configuration(proxyBeanMethods = false)
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+@ConfigurationProperties("authorization-server-security")
+public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Getter
+    @Setter
+    private String key = UUID.randomUUID().toString();
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
@@ -86,7 +82,7 @@ public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
     public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId("4sitive")
-                .clientSecret("secret")
+                .clientSecret("{noop}secret")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
                 .clientAuthenticationMethod(ClientAuthenticationMethod.POST)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
@@ -95,9 +91,10 @@ public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
                 .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
                 .redirectUri("http://127.0.0.1:8080/authorized")
                 .redirectUri("positive://login")
-                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.OPENID)
                 .scope("message.read")
                 .scope("message.write")
+                .tokenSettings(tokenSettings -> tokenSettings.accessTokenTimeToLive(Duration.ofDays(1L)))
 //                .clientSettings(clientSettings -> clientSettings.requireUserConsent(true))
                 .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
@@ -106,76 +103,31 @@ public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public JWKSource<SecurityContext> jwkSource() {
-        RSAKey rsaKey = generateRsa();
-        JWKSet jwkSet = new JWKSet(rsaKey);
+        OctetSequenceKey octetSequenceKey = new OctetSequenceKey.Builder(key.getBytes())
+                .keyID("sig")
+//                .keyUse(KeyUse.SIGNATURE)
+                .build();
+        JWKSet jwkSet = new JWKSet(octetSequenceKey);
         return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
 
     @Bean
     public ProviderSettings providerSettings() {
-        Map<String, Object> settings = new HashMap();
-        settings.put(ProviderSettings.AUTHORIZATION_ENDPOINT, "/oauth/authorize");
-        settings.put(ProviderSettings.TOKEN_ENDPOINT, "/oauth/token");
-        settings.put(ProviderSettings.JWK_SET_ENDPOINT, "/oauth/jwks");
-        settings.put(ProviderSettings.TOKEN_REVOCATION_ENDPOINT, "/oauth/revoke");
-        return new ProviderSettings(settings).issuer("https://api.4sitive.com");
+        return new ProviderSettings()
+                .authorizationEndpoint("/oauth/authorize")
+                .tokenEndpoint("/oauth/token")
+                .tokenRevocationEndpoint("/oauth/revoke")
+                .tokenIntrospectionEndpoint("/oauth/introspect")
+                .issuer("https://api.4sitive.com");
     }
 
-    public static RSAKey generateRsa() {
-        KeyPair keyPair = generateRsaKey();
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        return new RSAKey.Builder(publicKey)
-                .privateKey(privateKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 
-    static SecretKey generateSecretKey() {
-        SecretKey hmacKey;
-        try {
-            hmacKey = KeyGenerator.getInstance("HmacSha256").generateKey();
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return hmacKey;
-    }
-
-    static KeyPair generateRsaKey() {
-        KeyPair keyPair;
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            keyPair = keyPairGenerator.generateKeyPair();
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return keyPair;
-    }
-
-    static KeyPair generateEcKey() {
-        EllipticCurve ellipticCurve = new EllipticCurve(
-                new ECFieldFp(
-                        new BigInteger("115792089210356248762697446949407573530086143415290314195533631308867097853951")),
-                new BigInteger("115792089210356248762697446949407573530086143415290314195533631308867097853948"),
-                new BigInteger("41058363725152142129326129780047268409114441015993725554835256314039467401291"));
-        ECPoint ecPoint = new ECPoint(
-                new BigInteger("48439561293906451759052585252797914202762949526041747995844080717082404635286"),
-                new BigInteger("36134250956749795798585127919587881956611106672985015071877198253568414405109"));
-        ECParameterSpec ecParameterSpec = new ECParameterSpec(
-                ellipticCurve,
-                ecPoint,
-                new BigInteger("115792089210356248762697446949407573529996955224135760342422259061068512044369"),
-                1);
-
-        KeyPair keyPair;
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
-            keyPairGenerator.initialize(ecParameterSpec);
-            keyPair = keyPairGenerator.generateKeyPair();
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return keyPair;
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+        return context -> context.getHeaders().jwsAlgorithm(MacAlgorithm.HS256);
     }
 }
