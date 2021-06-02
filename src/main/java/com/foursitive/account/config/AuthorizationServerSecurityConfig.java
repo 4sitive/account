31f1d48f402e1f5fe3df65ddb74d7a54733c92da
이경욱 <kyungwook.yi@gmail.com>
2021-06-02 14:34:12 +0900

    JWK 스펙 추가

--- src/main/java/com/foursitive/account/config/AuthorizationServerSecurityConfig.java
+++ src/main/java/com/foursitive/account/config/AuthorizationServerSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.foursitive.account.config;
 
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.*;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import lombok.Getter;
@@ -14,7 +13,6 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
@@ -44,7 +42,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -56,6 +58,12 @@ public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdap
     @Getter
     @Setter
     private String key = UUID.randomUUID().toString();
+    @Getter
+    @Setter
+    private RSAPrivateKey privateKey;
+    @Getter
+    @Setter
+    private RSAPublicKey publicKey;
 
     public AuthorizationServerSecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
         this.clientRegistrationRepository = clientRegistrationRepository;
@@ -119,8 +127,8 @@ public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdap
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
-                .redirectUri("http://127.0.0.1:8080/authorized")
+                .redirectUri("http://127.0.0.1/login/oauth2/code/TEST")
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
                 .redirectUri("positive://login")
 //                .scope(OidcScopes.OPENID)
                 .scope("message.read")
@@ -134,11 +142,21 @@ public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdap
 
     @Bean
     public JWKSource<SecurityContext> jwkSource() {
-        OctetSequenceKey octetSequenceKey = new OctetSequenceKey.Builder(key.getBytes())
+        List<JWK> keys = new ArrayList<>();
+        keys.add(new OctetSequenceKey.Builder(key.getBytes())
                 .keyID("sig")
-//                .keyUse(KeyUse.SIGNATURE)
-                .build();
-        JWKSet jwkSet = new JWKSet(octetSequenceKey);
+                .keyUse(KeyUse.SIGNATURE)
+                .build());
+        Optional.ofNullable(publicKey)
+                .map(key -> {
+                    RSAKey.Builder builder = new RSAKey.Builder(key)
+                            .keyID("enc")
+                            .keyUse(KeyUse.ENCRYPTION);
+                    Optional.ofNullable(privateKey).ifPresent(builder::privateKey);
+                    return builder.build();
+                })
+                .ifPresent(keys::add);
+        JWKSet jwkSet = new JWKSet(keys);
         return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
 
@@ -149,7 +167,8 @@ public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdap
                 .tokenEndpoint("/oauth/token")
                 .tokenRevocationEndpoint("/oauth/revoke")
                 .tokenIntrospectionEndpoint("/oauth/introspect")
-                .issuer("https://api.4sitive.com");
+                .jwkSetEndpoint("/oauth/jwks")
+                .issuer("https://account.4sitive.com");
     }
 
     @Bean
