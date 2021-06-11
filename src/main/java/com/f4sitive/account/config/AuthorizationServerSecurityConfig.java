38528a25fd5340e73a1520f347e13249f011d9cb
이경욱 <kyungwook.yi@gmail.com>
2021-06-11 23:46:31 +0900

    op_policy_uri, op_tos_uri 추가

--- src/main/java/com/f4sitive/account/config/AuthorizationServerSecurityConfig.java
+++ src/main/java/com/f4sitive/account/config/AuthorizationServerSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.f4sitive.account.config;
 
-import com.nimbusds.jose.jwk.*;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import lombok.Getter;
@@ -11,6 +14,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
@@ -22,6 +28,9 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.OAuth2AuthorizationServerMetadata;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationResponseType;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AuthorizationServerMetadataHttpMessageConverter;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.JwtEncodingContext;
@@ -31,12 +40,18 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.web.OAuth2AuthorizationEndpointFilter;
+import org.springframework.security.oauth2.server.authorization.web.OAuth2AuthorizationServerMetadataEndpointFilter;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -89,6 +104,52 @@ public class AuthorizationServerSecurityConfig extends WebSecurityConfigurerAdap
                                     super.sendRedirect(request, response, url);
                                 }
                             });
+                        }
+                        if (object instanceof OAuth2AuthorizationServerMetadataEndpointFilter) {
+                            Field field = ReflectionUtils.findField(OAuth2AuthorizationServerMetadataEndpointFilter.class, "providerSettings");
+                            ReflectionUtils.makeAccessible(field);
+                            ProviderSettings providerSettings = (ProviderSettings) ReflectionUtils.getField(field, object);
+                            RequestMatcher requestMatcher = new AntPathRequestMatcher(OAuth2AuthorizationServerMetadataEndpointFilter.DEFAULT_OAUTH2_AUTHORIZATION_SERVER_METADATA_ENDPOINT_URI, HttpMethod.GET.name());
+                            OAuth2AuthorizationServerMetadataHttpMessageConverter authorizationServerMetadataHttpMessageConverter = new OAuth2AuthorizationServerMetadataHttpMessageConverter();
+                            return new OAuth2AuthorizationServerMetadataEndpointFilter(providerSettings) {
+                                @Override
+                                protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                                    if (!requestMatcher.matches(request)) {
+                                        filterChain.doFilter(request, response);
+                                        return;
+                                    }
+                                    OAuth2AuthorizationServerMetadata authorizationServerMetadata = OAuth2AuthorizationServerMetadata.builder()
+                                            .issuer(providerSettings.issuer())
+                                            .authorizationEndpoint(UriComponentsBuilder.fromUriString(providerSettings.issuer()).path(providerSettings.authorizationEndpoint()).toUriString())
+                                            .tokenEndpoint(UriComponentsBuilder.fromUriString(providerSettings.issuer()).path(providerSettings.tokenEndpoint()).toUriString())
+                                            .tokenEndpointAuthenticationMethods((authenticationMethods) -> {
+                                                authenticationMethods.add("client_secret_basic");
+                                                authenticationMethods.add("client_secret_post");
+                                            })
+                                            .jwkSetUrl(UriComponentsBuilder.fromUriString(providerSettings.issuer()).path(providerSettings.jwkSetEndpoint()).toUriString())
+                                            .responseType(OAuth2AuthorizationResponseType.CODE.getValue())
+                                            .grantType(AuthorizationGrantType.AUTHORIZATION_CODE.getValue())
+                                            .grantType(AuthorizationGrantType.CLIENT_CREDENTIALS.getValue())
+                                            .grantType(AuthorizationGrantType.REFRESH_TOKEN.getValue())
+                                            .tokenRevocationEndpoint(UriComponentsBuilder.fromUriString(providerSettings.issuer()).path(providerSettings.tokenRevocationEndpoint()).toUriString())
+                                            .tokenRevocationEndpointAuthenticationMethods((authenticationMethods) -> {
+                                                authenticationMethods.add("client_secret_basic");
+                                                authenticationMethods.add("client_secret_post");
+                                            })
+                                            .tokenIntrospectionEndpoint(UriComponentsBuilder.fromUriString(providerSettings.issuer()).path(providerSettings.tokenIntrospectionEndpoint()).toUriString())
+                                            .tokenIntrospectionEndpointAuthenticationMethods((authenticationMethods) -> {
+                                                authenticationMethods.add("client_secret_basic");
+                                                authenticationMethods.add("client_secret_post");
+                                            })
+                                            .codeChallengeMethod("plain")
+                                            .codeChallengeMethod("S256")
+                                            .claim("op_policy_uri", "https://cdn.4sitive.com/policy.html")
+                                            .claim("op_tos_uri", "https://cdn.4sitive.com/tos.html")
+                                            .build();
+                                    ServletServerHttpResponse httpResponse = new ServletServerHttpResponse(response);
+                                    authorizationServerMetadataHttpMessageConverter.write(authorizationServerMetadata, MediaType.APPLICATION_JSON, httpResponse);
+                                }
+                            };
                         }
                         return object;
                     }
