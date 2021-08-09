5564b200e2b593eaf215c6f41c941f6d04979dac
이경욱 <kyungwook.yi@gmail.com>
2021-08-09 22:52:25 +0900

    token cache 추가

--- src/main/java/com/f4sitive/account/config/CacheConfig.java
+++ src/main/java/com/f4sitive/account/config/CacheConfig.java
@@ -1,11 +1,13 @@
 package com.f4sitive.account.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.SerializationException;
@@ -15,6 +17,21 @@ import java.util.Collections;
 
 @Slf4j
 public class CacheConfig {
+    @Bean
+    Cache token(RedisConnectionFactory redisConnectionFactory) {
+        String name = "TOKEN";
+        RedisCacheManager cacheManager = RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(JsonNode.class)))
+                        .entryTtl(Duration.ofDays(20L))
+                        .computePrefixWith(cacheName -> "TK-"))
+                .initialCacheNames(Collections.singleton(name))
+                .disableCreateOnMissingCache()
+                .build();
+        cacheManager.afterPropertiesSet();
+        return cacheManager.getCache(name);
+    }
+
     @Bean
     Cache authorizationCode(RedisConnectionFactory redisConnectionFactory, ResourceLoader resourceLoader) {
         String name = "AUTHORIZATION_CODE";
