8446438f1762a0045a024ea6463bf5679511fb0b
이경욱 <kyungwook.yi@gmail.com>
2021-08-10 16:04:42 +0900

    snowflake id

--- src/main/java/com/f4sitive/account/entity/generator/SnowballGenerator.java
+++ src/main/java/com/f4sitive/account/entity/generator/SnowballGenerator.java
@@ -3,121 +3,111 @@ package com.f4sitive.account.entity.generator;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
-import org.springframework.util.Base64Utils;
+import org.springframework.util.StringUtils;
 
 import java.io.Serializable;
-import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
 import java.util.Collections;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SnowballGenerator implements IdentifierGenerator {
+    public static long GREGORIAN_EPOCH = -12219292800000L;
     private static final int UNUSED_BITS = 1;
     private static final int TIMESTAMP_BITS = 41;
     private static final int INSTANCE_BITS = 10;
-    private static final int SEQUENCE_BITS = 12; //long sequenceBits = 12L;
+    private static final int SEQUENCE_BITS = 12;
 
+    private static final long maxTimestamp = (1L << TIMESTAMP_BITS) - 1; // -1L ^ (-1L << INSTANCE_BITS);
     private static final long maxInstance = (1L << INSTANCE_BITS) - 1; // -1L ^ (-1L << INSTANCE_BITS);
     private static final long maxSequence = (1L << SEQUENCE_BITS) - 1;// -1L ^ (-1L << SEQUENCE_BITS);
 
-    private static final long DEFAULT_EPOCH = 1420070400000L; //twepoch = 1288834974657L;
+    private static final long DEFAULT_EPOCH = 1288834974657L; //LocalDate.of(2020, 01, 01).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli() twepoch = 1288834974657L;
 
     private final long instance;
-    private final long epoch;
 
     private volatile long lastTimestamp = -1L;
     private volatile long sequence = 0L;
 
-    public SnowballGenerator() throws SocketException, UnknownHostException {
-//        LocalDate.of()
-        System.out.println("maxInstance" +  maxInstance);
+    public SnowballGenerator() throws SocketException {
+
+        StringUtils.hasText(id) ? (id.hashCode() & maxInstance) : (new SecureRandom().nextLong() & maxInstance);
         long instance = instance();
         if (instance < 0 || instance > maxInstance) {
             throw new IllegalArgumentException(String.format("NodeId must be between %d and %d", 0, maxInstance));
         }
-        System.out.println(instance);
-//        this.instance = instance;
-        this.instance = instance & maxInstance | 1L;
-//        this.epoch = DEFAULT_EPOCH;
-        System.out.println("instance" + this.instance);
-        this.epoch = 0L;
+        this.instance = instance & maxInstance;
+        System.out.println("instance: " + Long.toBinaryString(this.instance));
 //        this.epoch = LocalDate.of(2020, 01, 01).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli();
     }
 
-    private long instance() throws UnknownHostException, SocketException {
-        long id = 0L;
-        for (byte x : InetAddress.getLocalHost().getAddress()) {
-            id = ((id << 8) - Byte.MIN_VALUE + x) & maxInstance;
-        }
-        System.out.println(new String(InetAddress.getLocalHost().getAddress()));
-//        System.out.println(new String(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress()));
-//        byte[] mac = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
-//        id = ((0x000000FF & (long) mac[mac.length - 1]) | (0x0000FF00 & (((long) mac[mac.length - 2]) << 8))) >> 6;
-        StringBuilder sb = new StringBuilder();
-//        return Optional.ofNullable(src)
-//                .map(Base64Utils::decodeFromUrlSafeString)
-//                .map(String::new)
-//                .map(token -> token.split("&"))
-//                .map(Arrays::asList)
-//                .orElse(Collections.emptyList())
-//                .stream()
-//                .map(name -> name.split("="))
-//                .filter(names -> names.length == 2)
-//                .collect(LinkedHashMap::new,
-//                        (map, names) -> map.put(names[0], names[1]),
-//                        Map::putAll);
-//        Collections.list(NetworkInterface.getNetworkInterfaces())
-//                .stream()
-//                .map(NetworkInterface::getHardwareAddress)
-//                .
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = networkInterfaces.nextElement();
-            byte[] mac = networkInterface.getHardwareAddress();
-            if (mac != null) {
-                for (byte macPort : mac) {
-                    sb.append(String.format("%02X", macPort));
-                }
-            }
-        }
-        System.out.println(sb);
-//    } catch (Exception ex) {
-//        nodeId = (new SecureRandom().nextInt());
-//    }
-//    nodeId = nodeId & maxNodeId;
-//        return nodeId;
-//        nodeId = sb.toString().hashCode();
-        return id;
+    private String instance() throws SocketException {
+        return Collections.list(NetworkInterface.getNetworkInterfaces())
+                .stream()
+                .filter(networkInterface -> {
+                    try {
+                        return !networkInterface.isLoopback() && !networkInterface.isPointToPoint() && !networkInterface.isVirtual() && networkInterface.isUp();
+                    } catch (SocketException e) {
+                        return false;
+                    }
+                })
+                .sorted()
+                .map(networkInterface -> {
+                    try {
+                        return networkInterface.getHardwareAddress();
+                    } catch (SocketException e) {
+                        return null;
+                    }
+                })
+                .map(hardwareAddress -> IntStream.generate(ByteBuffer.wrap(hardwareAddress)::get)
+                        .limit(hardwareAddress.length)
+                        .mapToObj(b -> String.format("%02X", (byte) b))
+                        .collect(Collectors.joining("-")))
+                .collect(Collectors.joining());
     }
 
     public Serializable generate(SharedSessionContractImplementor session, Object o) throws HibernateException {
         return Long.toString(generateId(), 36);
     }
 
+    public static long timestamp(UUID uuid) {
+        return (uuid.timestamp() / 10000) + GREGORIAN_EPOCH;
+    }
+
+    public static UUID uuid(long id) {
+        long timestamp = (id >> (INSTANCE_BITS + SEQUENCE_BITS)) + DEFAULT_EPOCH;
+        long instance = (id & (maxInstance << SEQUENCE_BITS)) >> SEQUENCE_BITS;
+        long sequence = id & maxSequence;
+        timestamp = ((timestamp - GREGORIAN_EPOCH) * 10000L);
+        long msb = 0L;
+        msb |= (0x00000000ffffffffL & timestamp) << 32;
+        msb |= (0x0000ffff00000000L & timestamp) >>> 16;
+        msb |= (0xffff000000000000L & timestamp) >>> 48;
+        msb |= 0x0000000000001000L;
+
+        long lsb = 0L;
+        lsb |= 0x8000000000000000L;
+        lsb |= (sequence & 0x0000000000003FFFL) << 48;
+        lsb |= instance | 0x0000010000000000L;
+        return new UUID(msb, lsb);
+    }
+
     public long[] parse(long id) {
-        long maskInstance = ((1L << INSTANCE_BITS) - 1) << SEQUENCE_BITS;
-        long maskSequence = (1L << SEQUENCE_BITS) - 1;
-        long timestamp = (id >> (INSTANCE_BITS + SEQUENCE_BITS)) + epoch;
-        long instance = (id & maskInstance) >> SEQUENCE_BITS;
-        long sequence = id & maskSequence;
-        return new long[]{timestamp, instance, sequence};
+        long timestamp = id >> (INSTANCE_BITS + SEQUENCE_BITS);
+        long instance = id & (maxInstance << SEQUENCE_BITS);
+        long sequence = id & maxSequence;
+        return new long[]{timestamp + DEFAULT_EPOCH, instance >> SEQUENCE_BITS, sequence};
     }
 
     public synchronized long generateId() {
-        long timestamp = System.currentTimeMillis(); //Instant.now().toEpochMilli()
-        if (timestamp < lastTimestamp) {
-            throw new IllegalStateException("Invalid System Clock!");
-        }
+        long timestamp = System.currentTimeMillis(); //Instant.now().toEpochMilli()  Clock.systemUTC().millis()
         if (lastTimestamp == timestamp) {
             sequence = (sequence + 1) & maxSequence;
             if (sequence == 0) {
-                System.out.println("EEEE");
                 do {
                     timestamp = System.currentTimeMillis();
                 } while (timestamp <= lastTimestamp);
@@ -126,6 +116,6 @@ public class SnowballGenerator implements IdentifierGenerator {
             sequence = 0;
         }
         lastTimestamp = timestamp;
-        return (timestamp - epoch) << (INSTANCE_BITS + SEQUENCE_BITS) | (instance << SEQUENCE_BITS) | sequence;
+        return (timestamp - DEFAULT_EPOCH) << (INSTANCE_BITS + SEQUENCE_BITS) | (instance << SEQUENCE_BITS) | sequence;
     }
 }
--- src/test/java/com/f4sitive/account/entity/generator/SnowballGeneratorTest.java
+++ src/test/java/com/f4sitive/account/entity/generator/SnowballGeneratorTest.java
@@ -3,52 +3,192 @@ package com.f4sitive.account.entity.generator;
 import com.f4sitive.account.util.ThreadUtils;
 import com.fasterxml.jackson.annotation.ObjectIdGenerator;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.uuid.Generators;
 import org.junit.jupiter.api.Test;
 
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SnowballGeneratorTest {
 
     @Test
-    void generateId() throws SocketException, UnknownHostException {
-        System.out.println(Instant.ofEpochMilli(1288834974657L));
-        System.out.println(LocalDate.of(2010, 11, 04));
-        System.out.println(LocalDate.of(2010, 11, 04).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli());
+    public void sssd() {
+        long sequence=0L;
+//        ThreadContext tctx = ThreadContext.getThreadContext();
+        long msb = 1L;
+        long lsb = (System.currentTimeMillis() - SnowballGenerator.GREGORIAN_EPOCH) * 1000 + ((sequence++) % 1000);
+        lsb = (lsb & 0xCFFFFFFFFFFFFFFFL) | (0x8000000000000000L);
+        msb = (msb & 0xFFFFFFFFFFFF0FFFL) | (0x0000000000001000L);
+
+//        long clock = new SecureRandom().nextLong();
+//        long lsb = 0;
+//        lsb |= 0x8000000000000000L;                 // variant (2 bits)
+//        lsb |= (clock & 0x0000000000003FFFL) << 48; // clock sequence (14 bits)
+//        lsb |= makeNode();                          // 6 bytes
+//        return lsb;
+        System.out.println(new Date(new UUID(msb, lsb).timestamp() / 10000L + 12219292800000L));
+        System.out.println(Instant.ofEpochMilli(TimeUnit.MICROSECONDS.toMillis((new UUID(msb, lsb).timestamp()-SnowballGenerator.GREGORIAN_EPOCH*1000)/10)));
+    }
+    @Test
+    public void time(){
+        long nanee = System.nanoTime();
+        System.out.println(TimeUnit.MILLISECONDS.toMicros(System.currentTimeMillis()));
+        System.out.println(TimeUnit.NANOSECONDS.toSeconds(nanee));
+        System.out.println((nanee % 1000L));
+        long time = (System.currentTimeMillis() * 1000L) + (System.nanoTime() % 1000L);
+//        long time = System.currentTimeMillis();
+        long nanos = (time - (time / 1000 * 1000)) * 10;
+System.out.println(nanos);
+System.out.println(Instant.ofEpochMilli(-12219292800000L));
+        long nanosSince = ((time/1000 + 12219292800000L) * 10000) + nanos;
+        System.out.println(Instant.ofEpochMilli(nanosSince));
+        long msb = 0L;
+        msb |= (0x00000000ffffffffL & nanosSince) << 32;
+        msb |= (0x0000ffff00000000L & nanosSince) >>> 16;
+        msb |= (0xffff000000000000L & nanosSince) >>> 48;
+        msb |= 0x0000000000001000L; // sets the version to 1.
+        long tet = (new UUID(msb,msb).timestamp() / 10) - 12219292800000L * 1000;
+
+System.out.println(Instant.ofEpochMilli(TimeUnit.MICROSECONDS.toMillis(tet)));
+System.out.println(Instant.ofEpochSecond(TimeUnit.NANOSECONDS.toSeconds(tet), TimeUnit.MICROSECONDS.toNanos(tet)));
+        System.out.println(Clock.systemUTC().instant());
+        System.out.println(Instant.ofEpochSecond(TimeUnit.NANOSECONDS.toSeconds(time), TimeUnit.NANOSECONDS.toNanos(time)));
+        System.out.println(Instant.ofEpochMilli(TimeUnit.NANOSECONDS.toMillis(time)));
+        System.out.println(Instant.ofEpochSecond(time/1000_000_000L, time%1000_000_000L));
+    }
+    @Test
+    public void sss() throws SocketException, UnknownHostException {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+        System.out.println(System.nanoTime());
+        System.out.println(Duration.between(Instant.EPOCH, Clock.systemUTC().instant()).getNano());
+        System.out.println(System.nanoTime() - Duration.between(Instant.EPOCH, Clock.systemUTC().instant()).getNano());
+        System.out.println(Instant.ofEpochMilli(System.nanoTime()/10));
+        System.out.println(System.currentTimeMillis() *1000_000 + System.nanoTime());
+//        1628445321476321108
+//        1628669025551654322
+        long  nanoss = System.currentTimeMillis()*1000_000+System.nanoTime();
+//        long  nanoss = System.currentTimeMillis() * 10E9 + System.nanoTime();
+        System.out.println("NANA"+nanoss);
+        Clock clock = Clock.systemDefaultZone();
+        Instant instant = clock.instant();   // or Instant.now();
+        long seconds = instant.getEpochSecond();
+        long nano = System.nanoTime()%1000_000_000;
+//        long nano = instant.getNano();
+//        new Timestamp(System.currentTimeMillis()).getNanos()
+        System.out.println(nano);
+        nanoss = seconds*1000_000_000 + nano;
+        System.out.println(seconds*1000_000_000 + nano);
+        System.out.println(instant);
+        System.out.println(System.nanoTime()%1000000000);
+        System.out.println(TimeUnit.MICROSECONDS.toNanos(Math.floorMod(System.nanoTime(), TimeUnit.SECONDS.toMicros(1))));
+        System.out.println(Instant.ofEpochMilli(seconds*1000_000_000 + nano));
+        System.out.println(Instant.ofEpochSecond(nanoss/1000_000_000L, nanoss%1000_000_000L).atOffset(ZoneOffset.UTC));
+        Generators.timeBasedGenerator().generate();
+        Calendar uuidEpoch = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        uuidEpoch.clear();
+        uuidEpoch.set(1582, 9, 15, 0, 0, 0); // 9 = October
+        long epochMillis = uuidEpoch.getTime().getTime();
+System.out.println("epochMillis" + epochMillis);
+//        long time = (uuid.timestamp() / 10000L) + epochMillis;
+
+        System.out.println(ZonedDateTime.of(1582, 10, 15, 0, 0, 0, 0, ZoneId.systemDefault()).toInstant().toEpochMilli());
         SnowballGenerator generator = new SnowballGenerator();
-        Map<Long, Long> result = new ConcurrentHashMap<>();
-        List<Long> data = new ArrayList<>();
+        long id = generator.generateId();
+        UUID parse = generator.to(id);
+        System.out.println("UUID:" + Instant.ofEpochMilli((parse.timestamp()-0x01b21dd213814000L)/10000));
+        System.out.println("UUID:" + Instant.ofEpochMilli(parse.timestamp()/10000L-12219323272000L));
+        System.out.println("UUID:" + Instant.ofEpochSecond(parse.timestamp()/10000000L));
+        System.out.println("UUID:" + parse.timestamp());
+        System.out.println("UUID:" + parse.variant());
+        System.out.println("UUID:" + parse.node());
+        System.out.println("UUID:" + parse.clockSequence());
+        System.out.println("UUID:" + Generators.timeBasedGenerator().generate().timestamp());
+        System.out.println("UUID:" + Generators.timeBasedGenerator().generate().variant());
+        System.out.println("UUID:" + Generators.timeBasedGenerator().generate().node());
+        System.out.println("UUID:" + Generators.timeBasedGenerator().generate().clockSequence());
+    }
+    @Test
+    public void test() throws SocketException, UnknownHostException {
+        System.out.println(Long.toBinaryString(0x0000FFFFFFFFFFFFL));
+        SnowballGenerator generator = new SnowballGenerator();
+        long id = generator.generateId();
+        long[] parse = generator.parse(id);
+        System.out.println("0:" + parse[0]);
+        System.out.println("1:" + parse[1]);
+        System.out.println("2:" + parse[2]);
+//        UUID:232379255050388
+//        UUID:0
+//        UUID:88600131211264
+//        UUID:755
+        System.out.println("UUID:" + new UUID(id, id).timestamp());
+        System.out.println("UUID:" + new UUID(id, id).variant());
+        System.out.println("UUID:" + new UUID(id, id).node());
+        System.out.println("UUID:" + new UUID(id, id).clockSequence());
+    }
+
+    private static long get64LeastSignificantBitsForVersion1() {
+        Random random = new Random();
+        long random63BitLong = random.nextLong() & 0x3FFFFFFFFFFFFFFFL;
+        long variant3BitFlag = 0x8000000000000000L;
+        return random63BitLong + variant3BitFlag;
+    }
+    public static UUID generateType1UUID() {
+        long most64SigBits = get64MostSignificantBitsForVersion1();
+        long least64SigBits = get64LeastSignificantBitsForVersion1();
+        return new UUID(most64SigBits, least64SigBits);
+    }
+    private static long get64MostSignificantBitsForVersion1() {
+//        Instant.
+        ZonedDateTime.of(1582, 10, 15, 0, 0, 0, 0, ZoneId.systemDefault()).toInstant().toEpochMilli();
+        LocalDateTime start = LocalDateTime.of(1582, 10, 15, 0, 0, 0);
+        Duration duration = Duration.between(start, LocalDateTime.now());
+        long timeForUuidIn100Nanos = duration.getSeconds() * 10000000 + duration.getNano() * 100;
+        long least12SignificatBitOfTime = (timeForUuidIn100Nanos & 0x000000000000FFFFL) >> 4;
+        long version = 1 << 12;
+        return (timeForUuidIn100Nanos & 0xFFFFFFFFFFFF0000L) + version + least12SignificatBitOfTime;
+    }
+
+    @Test
+    void generateId() throws SocketException, UnknownHostException {
+        SnowballGenerator generator = new SnowballGenerator();
         ThreadUtils.get(() -> {
-//            result.com
             Long id = generator.generateId();
-            long[] ss = generator.parse(id);
+            UUID uuid = generator.to(id);
+            System.out.println("UUID:" + uuid);
+            System.out.println("UUID:" + Instant.now());
+            System.out.println("UUIDtimestamp:" + Instant.ofEpochMilli((uuid.timestamp() / 10000) + SnowballGenerator.GREGORIAN_EPOCH));
+            System.out.println("UUIDtimestamp:" + uuid.timestamp());
+            System.out.println("UUIDvariant:" + uuid.variant());
+            System.out.println("UUIDnode:" + uuid.node());
+            System.out.println("UUIDclockSequence:" + uuid.clockSequence());
             System.out.println(System.currentTimeMillis());
-            System.out.println(ss[0]);
-            System.out.println(ss[1]);
-            System.out.println(ss[2]);
-            System.out.println(id);
-            System.out.println(Long.toString(id, 36));
-//            ObjectIdGenerator.IdKey
-            System.out.println((int)((id >> 12) & 0x0f));
-            System.out.println(new UUID(id,id).timestamp());
-//            5ec862f1-1c19-b000-5ec8-62f11c19b000
-//            5ec862fb-79d9-b000-5ec8-62fb79d9b000
-//            5ec8634c-9a19-b000-5ec8-634c9a19b000
-//            System.out.println(UUID.nameUUIDFromBytes(ByteBuffer.allocate(8).putLong(id).array()));
-//            System.out.println(UUID.fromString("f4c36093-ae5b-3762-97e1-59481d2f6fc8").timestamp());
             return id;
-        }, 1);
+        }, 10);
     }
 }
